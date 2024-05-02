7dfca595bf4cde4796a8be80d702698901704dd8
mszostek <mszostek@oakfusion.pl>
2024-05-02 16:56:38 +0200

    sdfs

---
+++ src/pages/pizza.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import "../styles/pizza.css";
+
+const PizzaPage = () => {
+  return <p className="">sdfgsdfgsd</p>;
+};
+
+export default PizzaPage;
+
+export const Head = () => <title>Pizza</title>;
